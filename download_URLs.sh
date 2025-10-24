6fd8866771ec563f09c080a3ca38e11cd86c5c4c
Miraj Ismail <mirajismail.2003@gmail.com>
2025-10-24 19:09:49 -0400

    fix file

--- download_URLs.sh
+++ download_URLs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-#!/bin/bash
 
-i=0
-while read -r url; do
-    ((i++))
+export check_url
+check_url() {
+    local i="$1"
+    local url="$2"
     wget -q --spider "$url" || echo "$i $url"
-done < URLsFile.txt | parallel -j $(nproc)
+}
+export -f check_url
+
+# Run in parallel, one job per URL line
+cat -n URLsFile.txt | parallel -j "$(nproc)" --colsep '\t' check_url {1} {2}
 
